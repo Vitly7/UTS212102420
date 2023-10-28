edfffc2bb43924d4204aca90e055e714cc828e64
Vitly7 <111552374+Vitly7@users.noreply.github.com>
2023-10-28 09:03:31 +0700

    done

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    EcommerceComponent,
+    FooterComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  templateUrl: './ecommerce.component.html',
+  styleUrls: ['./ecommerce.component.css']
+})
+export class EcommerceComponent {
+  nama = 'Vittorio Fiorentino';
+  nim = '212102420';
+}
